b721dd893c006f0e0f95aee8a802d9d4b85525af
Franck <snake386@hotmail.com>
2018-12-12 08:22:50 -0500

    updated

--- src/Controller/AbstractController.ts
+++ src/Controller/AbstractController.ts
@@ -2,15 +2,9 @@ import {Request, Response} from "express"
 
 export abstract class AbstractController
 {
-    /**
-     * @param {e.Request} request
-     * @param {e.Response} response
-     * @param next
-     */
     constructor(
         protected request: Request,
         protected response: Response,
         protected next: any,
     ) {}
-
 }
--- src/Middleware/ErrorHandlerMiddleware.ts
+++ src/Middleware/ErrorHandlerMiddleware.ts
@@ -3,24 +3,14 @@ import {MiddlewareErrorHandlerInterface} from "../Util/Middleware/MiddlewareErro
 
 class ErrorHandlerMiddleware implements MiddlewareErrorHandlerInterface
 {
-    /**
-     * @param err
-     * @param request
-     * @param response
-     * @param next
-     */
-    public resolve(err: any, request: Request, response: Response, next: any): any
-    {
+    public resolve(err: any, request: Request, response: Response, next: any): any {
         let msg = "Something broke!"
         if (process.env.NODE_ENV === 'development') {
             msg = err.toString()
         } else {
             console.error(err.stack)
         }
-        response.status(500).json({
-            "status": 500,
-            "message": msg
-        })
+        response.status(500).send(msg)
     }
 }
 
